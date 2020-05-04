0e8b18e1bf55f4bd7283df5d0d6e5fcff394aab6
JessicaCoelho21 <44201718+JessicaCoelho21@users.noreply.github.com>
2020-05-04 15:15:29 +0100

    Erros na route do user/usercontroller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,7 +1,9 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt');
+//const jwt = require('jsonwebtoken');
 
 //Código em falta
-exports.createUser = (req, res, next) => {
+const createUser = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
@@ -27,8 +29,9 @@ exports.createUser = (req, res, next) => {
         });
 }
 
-exports.loginUser = (req, res, next) => {
+const loginUser = (req, res, next) => {
     let fetchedUser;
+
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
@@ -39,28 +42,35 @@ exports.loginUser = (req, res, next) => {
             fetchedUser = user;
             return bcrypt.compare(req.body.password, user.password);
         })
-        .then(result => {
-            if (!result) {
-                return res.status(401).json({
-                    message: 'Auth failed'
-                });
-            }
-            const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id },
-                'segredo_para_a_criacao_dos_tokens', { expiresIn: '30m' }
-            );
-            res.status(200).json({
-                token,
-                expiresIn: 1800,
-                userId: fetchedUser._id,
-                tipo: fetchedUser.tipo
-            });
-        }).catch(err => {
+
+    .then(result => {
+        if (!result) {
             return res.status(401).json({
                 message: 'Auth failed'
             });
+        }
+
+        const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id },
+            'segredo_para_a_criacao_dos_tokens', { expiresIn: '30m' }
+        );
+
+        res.status(200).json({
+            token,
+            expiresIn: 1800,
+            userId: fetchedUser._id,
+            tipo: fetchedUser.tipo
         });
+    })
+
+    .catch(err => {
+        return res.status(401).json({
+            message: 'Auth failed'
+        });
+    });
 }
 
 module.exports = {
     //Código em falta
+    createUser,
+    loginUser
 }
--- api/routes/users-router.js
+++ api/routes/users-router.js
@@ -4,5 +4,8 @@ const userController = require('../controllers/userController')
 const userRouter = express.Router()
 
 //Código em falta
+userRouter.post('/signup', userController.createUser);
+
+userRouter.post('/login', userController.loginUSer);
 
 module.exports = userRouter
