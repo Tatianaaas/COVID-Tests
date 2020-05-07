d215e6d6978331d0795dfa9c83152d41bebbca38
JessicaCoelho21 <44201718+JessicaCoelho21@users.noreply.github.com>
2020-05-07 16:31:47 +0100

    Autorização não funciona

--- api/routes/users-router.js
+++ api/routes/users-router.js
@@ -9,14 +9,13 @@ userRouter.post('/signup', userController.createUser);
 userRouter.post('/signup', userController.createUser);
 
 //Login
-//userRouter.get('/login',userController.getLog)
 userRouter.post('/login', userController.loginUser);
 
 //vou colocar aqui as rotas que sao responsaveis pelo criar, editar, atualizar e remover utlizidador
 //como o create ja esta a ser utilizado no signup , penso que nao preciso de colocar outra vez
 //Ver um determinado utilizador pelo ID
-userRouter.get("/show/:userId", /*verifyJWT,*/ userController.getUserById);
+userRouter.get("/show/:userId", userController.getUserById);
 //Editar utilizador
-userRouter.put("/update/:userId", /*verifyJWT,*/ userController.updateUser);
+userRouter.put("/update/:userId", userController.updateUser);
 
 module.exports = userRouter
